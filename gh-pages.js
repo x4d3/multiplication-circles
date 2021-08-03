6ecfa8ba88e84d0d3febab579f2dcf69e89ee9c7
Xavier Delamotte <xavier.delamotte@red-badger.com>
2021-08-03 21:33:05 +0100

    initial gh-pages commit

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+  'build', // path to public directory
+  {
+    branch: 'gh-pages',
+    repo: 'https://github.com/x4d3/svelte-multiplication-circles.git', // Update to point to your repository
+    user: {
+      name: 'Xavier Delamotte', // update to use your name
+      email: 'git@xade.eu' // Update to use your email
+    }
+  },
+  () => {
+    console.log('Deploy Complete!')
+  }
+)
