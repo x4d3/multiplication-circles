d46a2c6a504fa59164c0d90a4a30c5b79ca580bb
Xavier Delamotte <xavier.delamotte@red-badger.com>
2021-08-04 17:50:33 +0100

    Remove double click zoom

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -5,7 +5,6 @@
 	export let count = 0;
 
 	const displayed_count = spring();
-	$: displayed_count.set(count);
 	$: offset = modulo($displayed_count, 1);
 
 	function modulo(n: number, m: number) {
@@ -15,7 +14,7 @@
 </script>
 
 <div class="counter">
-	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
+	<button on:click={() => {count -= 1;displayed_count.set(count);}} aria-label="Decrease the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5" />
 		</svg>
@@ -28,7 +27,7 @@
 		</div>
 	</div>
 
-	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
+	<button on:click={() => {count += 1;displayed_count.set(count);}} aria-label="Increase the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
 		</svg>
@@ -37,6 +36,7 @@
 
 <style>
 	.counter {
+        touch-action: manipulation;
 		display: flex;
 		border-top: 1px solid rgba(0, 0, 0, 0.1);
 		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
